d706a234dad6cdc14198cd477cff3318ae528453
Nurfikri Ihsan <nurfikriihsan@gmail.com>
2025-07-31 13:48:44 +0700

    Developer

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://jsonplaceholder.typicode.com/posts';
-
-export const fetchComments = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
-};
